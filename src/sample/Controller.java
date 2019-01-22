2108974878b2e9ecbfb71d838484131af244c699
3yebmb <3yebmb@gmail.com>
2019-01-22 10:14:37 +0500

    add eventы and handlerы

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,32 @@
 package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 //<GridPane alignment="center" hgap="10" vgap="10"
 public class Controller {
+    @FXML
+    Button addBtn;
+
+    @FXML
+    Button updBtn;
+
+    @FXML
+    Button remBtn;
+
+    public void addQuery(){
+        ;
+    }
+
+    public void remQuery(){
+        ;
+    }
+
+    public void updQuery(){
+        ;
+    }
+
+    private void selectQuery(){
+        
+    }
 }
